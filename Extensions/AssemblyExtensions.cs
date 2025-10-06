c43f48493df7e6405f997aff43f1b5cb3019d976
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-06 11:11:17 +0800

    refactor(Extensions): Simplify `GetEmbeddedResource` signature and update LangVersion to `preview`

--- Extensions/AssemblyExtensions.cs
+++ Extensions/AssemblyExtensions.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Reflection;
 
@@ -6,18 +5,14 @@ namespace BepInExUtils.Generator.Extensions;
 
 public static class AssemblyExtensions
 {
-    /// <summary>
-    ///     Retrieves the content of an embedded resource from an assembly as a string.
-    /// </summary>
-    /// <param name="assembly">The assembly containing the embedded resource.</param>
-    /// <param name="resource">The name of the embedded resource.</param>
-    /// <returns>The content of the embedded resource as a string.</returns>
-    /// <exception cref="Exception">Thrown when the specified embedded resource is not found in the assembly.</exception>
-    public static string GetEmbeddedResource(this Assembly assembly, string resource)
+    extension(Assembly assembly)
     {
-        using var resourceStream = new StreamReader(assembly.GetManifestResourceStream(resource) ??
-                                                    throw new(
-                                                        $"Failed to find EmbeddedResource '{resource}' in Assembly '{assembly}' (Available Resources: {string.Join(", ", assembly.GetManifestResourceNames())})"));
-        return resourceStream.ReadToEnd();
+        public string GetEmbeddedResource(string resource)
+        {
+            using var resourceStream = new StreamReader(assembly.GetManifestResourceStream(resource) ??
+                                                        throw new(
+                                                            $"Failed to find EmbeddedResource '{resource}' in Assembly '{assembly}' (Available Resources: {string.Join(", ", assembly.GetManifestResourceNames())})"));
+            return resourceStream.ReadToEnd();
+        }
     }
 }
